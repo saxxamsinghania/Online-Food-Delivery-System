3ce70e6a3cd9b5323f7195d322f448b247cb02b0
saxxamsinghania <saksham1225@ce.du.ac.in>
2025-01-19 15:41:07 +0530

    requirements edited

--- food_delivery_app_v2/app/__init__.py
+++ food_delivery_app_v2/app/__init__.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mysqldb import MySQL
 from flask_migrate import Migrate
-# from flask_jwt_extended import JWTManager
 from .config import Config
 
 mysql = MySQL()
@@ -12,19 +11,16 @@ mysql = MySQL()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
-# jwt = JWTManager()
 migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    # mysql = MySQL(app)
     mysql.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
-    # jwt.init_app(app)
-    
+        
     from .routes import auth, customer, restaurant, order, home
     app.register_blueprint(home.bp)
     app.register_blueprint(auth.bp)
--- food_delivery_app_v2/app/config.py
+++ food_delivery_app_v2/app/config.py
@@ -10,10 +10,3 @@ class Config:
     MYSQL_CURSORCLASS = 'DictCursor'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/online_food_delivery_3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    # # JWT Configuration
-    # JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', SECRET_KEY)
-    # JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(hours=24)
-    # JWT_BLACKLIST_ENABLED = True
-    # JWT_BLACKLIST_TOKEN_CHECKS = ['access']
-
