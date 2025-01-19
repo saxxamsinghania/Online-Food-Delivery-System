377d78d72545f69c80787d4b71cf116c954cc8d0
Saksham Singhania <151566210+saxxamsinghania@users.noreply.github.com>
2025-01-20 02:41:04 +0530

    Update config.py

--- food_delivery_app_v2/app/config.py
+++ food_delivery_app_v2/app/config.py
@@ -4,8 +4,8 @@ class Config:
     SECRET_KEY = 'your-secret-key-here'
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'password'
+    MYSQL_PASSWORD = 'password'        #replace here with your password
     MYSQL_DB = 'online_food_delivery_3'
     MYSQL_CURSORCLASS = 'DictCursor'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/online_food_delivery_3'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/online_food_delivery_3'    #here in place of password
     SQLALCHEMY_TRACK_MODIFICATIONS = False
