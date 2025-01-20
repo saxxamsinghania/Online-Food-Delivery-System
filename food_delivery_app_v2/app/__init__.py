eab0166970bb0a19724021180b4b2464def27d92
Saksham Singhania <151566210+saxxamsinghania@users.noreply.github.com>
2025-01-20 23:57:32 +0530

    Update __init__.py

--- food_delivery_app_v2/app/__init__.py
+++ food_delivery_app_v2/app/__init__.py
@@ -2,11 +2,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_mysqldb import MySQL
 from flask_migrate import Migrate
 from .config import Config
-
-mysql = MySQL()
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -16,16 +13,18 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    mysql.init_app(app)
+    
+    # Initialize extensions
     db.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
-        
+
+    # Register blueprints
     from .routes import auth, customer, restaurant, order, home
     app.register_blueprint(home.bp)
     app.register_blueprint(auth.bp)
     app.register_blueprint(customer.bp)
     app.register_blueprint(restaurant.bp)
     app.register_blueprint(order.bp)
-
+    
     return app
