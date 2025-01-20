e65c061e0786ca8e56f3eaa014ee1d6f53abe200
Saksham Singhania <151566210+saxxamsinghania@users.noreply.github.com>
2025-01-21 02:51:49 +0530

    Update auth.py

--- food_delivery_app_v2/app/routes/auth.py
+++ food_delivery_app_v2/app/routes/auth.py
@@ -3,7 +3,6 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.models import Customer
-from app import mysql
 from app import db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
