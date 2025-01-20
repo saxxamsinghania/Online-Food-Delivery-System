c4941d59aede24d2a77fe05bf60a50bc7477bf38
Saksham Singhania <151566210+saxxamsinghania@users.noreply.github.com>
2025-01-21 05:00:14 +0530

    Update config.py

--- food_delivery_app_v2/app/config.py
+++ food_delivery_app_v2/app/config.py
@@ -1,5 +1,5 @@
 # config.py
 class Config:
     SECRET_KEY = 'your-secret-key-here'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/online_food_delivery_3'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/online_food_delivery'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
