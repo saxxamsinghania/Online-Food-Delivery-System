5ae7d49150bdfb67496d28e45d0c49675f0ee957
Saksham Singhania <151566210+saxxamsinghania@users.noreply.github.com>
2025-01-21 02:53:49 +0530

    Update home.py

--- food_delivery_app_v2/app/routes/home.py
+++ food_delivery_app_v2/app/routes/home.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.models import Customer
-from app import mysql
 
 bp = Blueprint('home', __name__)
 
